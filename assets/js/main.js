8a8a9b10666be203709ac97099fe538bf7644f64
ramaherdianto <ramadhanherdianto35@gmail.com>
2022-02-03 21:09:58 +0700

    manambahkan file

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+// =========== Preloader
+(function ($) {
+  "use strict";
+
+  // Preloader
+  $(window).on("load", function () {
+    if ($("#preloader").length) {
+      $("#preloader")
+        .delay(100)
+        .fadeOut("slow", function () {
+          $(this).remove();
+        });
+    }
+  });
+})(jQuery);
