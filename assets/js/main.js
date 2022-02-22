f77d22383c91fd217b7b0c36148105adba4c2a4d
ramaherdianto <ramadhanherdianto35@gmail.com>
2022-02-22 13:45:38 +0700

    menambahkan animasi navbar ketika discroll menjadi memiliki warna dan mempercantik button kirim di contact form

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,26 @@
 // =========== Preloader
 (function ($) {
-   "use strict";
+  "use strict";
 
-   // Preloader
-   $(window).on("load", function () {
-      if ($("#loader").length) {
-         $("#loader")
-            .delay(100)
-            .fadeOut("slow", function () {
-               $(this).remove();
-            });
-      }
-   });
+  // Preloader
+  $(window).on("load", function () {
+    if ($("#loader").length) {
+      $("#loader")
+        .delay(100)
+        .fadeOut("slow", function () {
+          $(this).remove();
+        });
+    }
+  });
 })(jQuery);
+
+// Transition navbar
+$(window).on("scroll", function () {
+  if ($(window).scrollTop() > 50) {
+    $(".navbar").addClass("active");
+    $(".navbar").addClass("shadow-sm");
+  } else {
+    $(".navbar").removeClass("active");
+    $(".navbar").removeClass("shadow-sm");
+  }
+});
