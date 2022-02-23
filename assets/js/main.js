a0d3bf889b3811d743d566fc96a470b94c5b8928
ramaherdianto <ramadhanherdianto35@gmail.com>
2022-02-23 12:56:15 +0700

    merubah logo, update button kirim dan menambah padding setiap section

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,8 +4,8 @@
 
   // Preloader
   $(window).on("load", function () {
-    if ($("#loader").length) {
-      $("#loader")
+    if ($("#preloader").length) {
+      $("#preloader")
         .delay(100)
         .fadeOut("slow", function () {
           $(this).remove();
