f4107d457cc2237670501c67e83ad2cd3a659629
ramaherdianto <ramadhanherdianto35@gmail.com>
2022-03-24 19:59:53 +0700

    menambahkan section footer belum responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,3 +24,33 @@ $(window).on("scroll", function () {
     $(".navbar").removeClass("shadow-sm");
   }
 });
+
+// Function ContactForm
+const scriptURL = "https://script.google.com/macros/s/AKfycby6yqBLRi8c7ePJRyjCMD44_PHUinwD5FucICOpsh2E-wgcF3DzRHSuhpV0fqlb-rYXgA/exec";
+const form = document.forms["contact-form"];
+const btnKirim = document.querySelector(".btn-kirim");
+const btnLoading = document.querySelector(".btn-loading");
+const myAlert = document.querySelector(".my-alert");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // ketika tombol submit di klik
+  // tampilkan tombol loading, hilangkan tombol kirim
+  btnLoading.classList.toggle("d-none");
+  btnKirim.classList.toggle("d-none");
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      btnLoading.classList.toggle("d-none");
+      btnKirim.classList.toggle("d-none");
+      // tampilkan alert
+      myAlert.classList.toggle("d-none");
+      // reset form
+      form.reset();
+      console.log("Success!", response);
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
+
+// Tahun Copyright
+let copyright = new Date();
+document.getElementById("copyright").innerHTML = "© " + copyright.getFullYear() + " <b>TeknologiU.</b> " + "All rights reserved.";
