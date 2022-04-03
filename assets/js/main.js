49ac39400551adace666a54fb90f2a17a4cee1f6
ramaherdianto <ramadhanherdianto35@gmail.com>
2022-04-04 05:32:07 +0700

    merapihkan code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,28 @@
 // =========== Preloader
 (function ($) {
-   "use strict";
+  "use strict";
 
-   // Preloader
-   $(window).on("load", function () {
-      if ($("#preloader").length) {
-         $("#preloader")
-            .delay(100)
-            .fadeOut("slow", function () {
-               $(this).remove();
-            });
-      }
-   });
+  // Preloader
+  $(window).on("load", function () {
+    if ($("#preloader").length) {
+      $("#preloader")
+        .delay(100)
+        .fadeOut("slow", function () {
+          $(this).remove();
+        });
+    }
+  });
 })(jQuery);
 
 // Transition navbar
 $(window).on("scroll", function () {
-   if ($(window).scrollTop() > 50) {
-      $(".navbar").addClass("active");
-      $(".navbar").addClass("shadow-sm");
-   } else {
-      $(".navbar").removeClass("active");
-      $(".navbar").removeClass("shadow-sm");
-   }
+  if ($(window).scrollTop() > 50) {
+    $(".navbar").addClass("active");
+    $(".navbar").addClass("shadow-sm");
+  } else {
+    $(".navbar").removeClass("active");
+    $(".navbar").removeClass("shadow-sm");
+  }
 });
 
 // Function ContactForm
@@ -33,22 +33,22 @@ const btnLoading = document.querySelector(".btn-loading");
 const myAlert = document.querySelector(".my-alert");
 
 form.addEventListener("submit", (e) => {
-   e.preventDefault();
-   // ketika tombol submit di klik
-   // tampilkan tombol loading, hilangkan tombol kirim
-   btnLoading.classList.toggle("d-none");
-   btnKirim.classList.toggle("d-none");
-   fetch(scriptURL, { method: "POST", body: new FormData(form) })
-      .then((response) => {
-         btnLoading.classList.toggle("d-none");
-         btnKirim.classList.toggle("d-none");
-         // tampilkan alert
-         myAlert.classList.toggle("d-none");
-         // reset form
-         form.reset();
-         console.log("Success!", response);
-      })
-      .catch((error) => console.error("Error!", error.message));
+  e.preventDefault();
+  // ketika tombol submit di klik
+  // tampilkan tombol loading, hilangkan tombol kirim
+  btnLoading.classList.toggle("d-none");
+  btnKirim.classList.toggle("d-none");
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      btnLoading.classList.toggle("d-none");
+      btnKirim.classList.toggle("d-none");
+      // tampilkan alert
+      myAlert.classList.toggle("d-none");
+      // reset form
+      form.reset();
+      console.log("Success!", response);
+    })
+    .catch((error) => console.error("Error!", error.message));
 });
 
 // Tahun Copyright
@@ -57,7 +57,7 @@ document.getElementById("copyright").innerHTML = "© " + copyright.getFullYear()
 
 // Aos JS
 AOS.init({
-   once: true,
+  once: true,
 });
 
 // Hero Animasi Typing
@@ -65,11 +65,11 @@ let i = 0;
 let text = "Halo Uceers, Selamat Datang!";
 
 const typing = () => {
-   if (i < text.length) {
-      document.querySelector(".typing").innerHTML += text.charAt(i);
-      i++;
-      setTimeout(typing, 110);
-   }
+  if (i < text.length) {
+    document.querySelector(".typing").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typing, 110);
+  }
 };
 
 typing();
